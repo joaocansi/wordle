4276318c6a97fc07878ccd0b7d402c2dc491e186
joaocansi <whitshs2003@gmail.com>
2023-02-27 19:50:31 -0300

    fix: useSyncState typed correctly

--- src/hooks/useSyncState.tsx
+++ src/hooks/useSyncState.tsx
@@ -5,7 +5,6 @@ export const useSyncState = <T,>(
 ): [() => T, (newState: T) => void] => {
   const [state, updateState] = useState(initialState);
   let current = state;
-
   const get = (): T => current;
   const set = (newState: T) => {
     current = newState;
